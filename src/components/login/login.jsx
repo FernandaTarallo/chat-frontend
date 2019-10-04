d9faebb910e83b32968ab0e7f6826c148adfac8c
FernandaTarallo <tarallofernanda@gmail.com>
2019-10-03 22:21:18 -0300

    integração registro ok

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Logo from '../../icon.png'
 import BackImage from '../../background-image.jpg'
-import { Link } from 'react-router-dom';
+import axios from 'axios'
+import { Redirect } from 'react-router-dom';
+import Swal from 'sweetalert2'
+
 
 export default class Login extends React.Component {
     constructor(props){
@@ -9,53 +12,123 @@ export default class Login extends React.Component {
         
         this.state = {
             register: false,
+            email: '',
+            username: '',
+            password: '',
             classForm: "",
         }
+    }
+
+    register = async(event) => {
+
+        event.preventDefault()
+
+        axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+        
+        try{
+            const register = await axios.post('http://localhost:4000/users', {
+                email: this.state.email,
+                username: this.state.username,
+                password: this.state.password
+            });
+            
+            this.switchMode()
+
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top',
+                showConfirmButton: false,
+                timer: 3000
+              })
+              
+              Toast.fire({
+                type: 'success',
+                title: 'Cadastro efetuado com sucesso.'
+              })
+            console.log("Certou"+JSON.stringify(register.data));
+        }catch(err){
+            console.log(JSON.stringify(err));
+        }
+    }
+
+    changeHandler = event => {
+
+        const name = event.target.name
+        
+        this.setState({
+            
+            [name]: event.target.value
+            
+        })
+    }
+
+    switchMode = () => {
+        this.setState({
+            register: !this.state.register,
+            email: '',
+            password: '',
+            username: ''
+        })
+    }
+
+    login = async(event) => {
+
+        event.preventDefault()
+
+        console.log('Foi');
+        return <Redirect to='/'/>
+       
     }
 
     render(){
         return(
             <div className="box">
+
                 <div className="container d-flex justify-content-center">
-                    
                         <div className="form-box bg-transparent col-md-4">
                             <div className="justify-content-center d-flex mb-5">
                                 <i className="material-icons text-white user-big-icon">person_outline</i>
                             </div>
-                            <form>
-                                <div className="form-group mb-4">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text bg-transparent">
-                                                <i class="material-icons text-white">person_outline</i>
-                                            </span>
+                            <form onSubmit={this.register} method="POST">
+                                {this.state.register && 
+                                    <div className="form-group mb-4">
+                                        <div className="input-group">
+                                            <div className="input-group-prepend">
+                                                <span className="input-group-text bg-transparent">
+                                                    <i className="material-icons text-white">person_outline</i>
+                                                </span>
+                                            </div>
+                                            <input onChange={this.changeHandler} name="username" type="text" className="form-control form-control-lg" value={this.state.username} placeholder="Enter username"/>
                                         </div>
-                                        <input type="text" className="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="userHelp" placeholder="Enter username"/>
                                     </div>
-                                </div>
+                                }
                                 <div className="form-group mb-4">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text bg-transparent">
-                                                <i class="material-icons text-white">mail_outline</i>
+                                    <div className="input-group">
+                                        <div className="input-group-prepend">
+                                            <span className="input-group-text bg-transparent">
+                                                <i className="material-icons text-white">mail_outline</i>
                                             </span>
                                         </div>
-                                        <input type="email" className="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                                        <input onChange={this.changeHandler} name="email" type="email" className="form-control form-control-lg" value={this.state.email} placeholder="Enter email"/>
                                     </div>
                                 </div>
 
                                 <div className="form-group mb-4">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text bg-transparent">
-                                                <i class="material-icons text-white">lock_outline</i>
+                                    <div className="input-group">
+                                        <div className="input-group-prepend">
+                                            <span className="input-group-text bg-transparent">
+                                                <i className="material-icons text-white">lock_outline</i>
                                             </span>
                                         </div>
-                                        <input type="password" className="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password"/>
+                                        <input onChange={this.changeHandler} name="password" type="password" className="form-control form-control-lg" value={this.state.password} placeholder="Password"/>
                                     </div>
                                 </div>
                                 <div className="text-center">
-                                    <button type="submit" className="btn btn-lg btn-register btn-block">Registrar</button>
+                                    <button onClick={this.state.register ? this.register : this.login} className="btn btn-lg btn-register btn-block">{this.state.register ? 'Registrar' : 'Logar'}</button>
+                                    {this.state.register
+                                        ? <p onClick={this.switchMode}>Clique aqui para efetuar o login.</p>
+                                        : <p onClick={this.switchMode}>Não possui uma conta? Cadastre-se!</p>
+                                    }
                                 </div>
                                 <hr className="white-line"/>
                             </form>
